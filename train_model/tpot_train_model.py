b7a9c2210a4ceb241788aad5ff0578cdb64e1d07
zwdq <qchannel@126.com>
2021-03-15 15:11:08 +0800

    123

--- train_model/tpot_train_model.py
+++ train_model/tpot_train_model.py
@@ -1,10 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.tree import DecisionTreeClassifier
-from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,11 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.8218322580645161
-exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.5, min_samples_leaf=8, min_samples_split=19, n_estimators=100)),
-    DecisionTreeClassifier(criterion="gini", max_depth=3, min_samples_leaf=18, min_samples_split=14)
-)
+# Average CV score on the training set was: 0.8121806451612903
+exported_pipeline = GradientBoostingClassifier(learning_rate=0.1, max_depth=3, max_features=0.8500000000000001, min_samples_leaf=7, min_samples_split=20, n_estimators=100, subsample=0.8500000000000001)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
